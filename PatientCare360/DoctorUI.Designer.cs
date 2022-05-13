05c112fdc751c0f0fc35cd623a7ab9c5287150c8
Dori Fourer <dori.fourer654@gmail.com>
2022-05-13 18:36:00 +0300

    a

--- PatientCare360/DoctorUI.Designer.cs
+++ PatientCare360/DoctorUI.Designer.cs
@@ -30,14 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DoctorUI));
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.btnHome = new System.Windows.Forms.Button();
-            this.btnNewPat = new System.Windows.Forms.Button();
-            this.btnPatHistory = new System.Windows.Forms.Button();
             this.btnLogOut = new System.Windows.Forms.Button();
-            this.panelLeft = new System.Windows.Forms.Panel();
+            this.btnPatHistory = new System.Windows.Forms.Button();
+            this.btnNewPat = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panelLeft = new System.Windows.Forms.Panel();
             this.panel1.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
@@ -53,58 +53,24 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(283, 948);
+            this.panel1.Size = new System.Drawing.Size(283, 736);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // btnLogOut
             // 
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel2.Location = new System.Drawing.Point(283, 798);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(1325, 150);
-            this.panel2.TabIndex = 1;
-            // 
-            // panel3
-            // 
-            this.panel3.BackColor = System.Drawing.Color.White;
-            this.panel3.Controls.Add(this.pictureBox1);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
-            this.panel3.Location = new System.Drawing.Point(0, 0);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(283, 103);
-            this.panel3.TabIndex = 2;
-            // 
-            // btnHome
-            // 
-            this.btnHome.FlatAppearance.BorderSize = 0;
-            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnHome.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnHome.ForeColor = System.Drawing.Color.White;
-            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
-            this.btnHome.Location = new System.Drawing.Point(0, 109);
-            this.btnHome.Name = "btnHome";
-            this.btnHome.Size = new System.Drawing.Size(280, 108);
-            this.btnHome.TabIndex = 3;
-            this.btnHome.Text = "Home";
-            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnHome.UseVisualStyleBackColor = true;
-            this.btnHome.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // btnNewPat
-            // 
-            this.btnNewPat.FlatAppearance.BorderSize = 0;
-            this.btnNewPat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnNewPat.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnNewPat.ForeColor = System.Drawing.Color.White;
-            this.btnNewPat.Image = ((System.Drawing.Image)(resources.GetObject("btnNewPat.Image")));
-            this.btnNewPat.Location = new System.Drawing.Point(3, 223);
-            this.btnNewPat.Name = "btnNewPat";
-            this.btnNewPat.Size = new System.Drawing.Size(277, 108);
-            this.btnNewPat.TabIndex = 4;
-            this.btnNewPat.Text = "New Patient";
-            this.btnNewPat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnNewPat.UseVisualStyleBackColor = true;
-            this.btnNewPat.Click += new System.EventHandler(this.btnNewPat_Click);
+            this.btnLogOut.FlatAppearance.BorderSize = 0;
+            this.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLogOut.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogOut.ForeColor = System.Drawing.Color.White;
+            this.btnLogOut.Image = ((System.Drawing.Image)(resources.GetObject("btnLogOut.Image")));
+            this.btnLogOut.Location = new System.Drawing.Point(3, 587);
+            this.btnLogOut.Name = "btnLogOut";
+            this.btnLogOut.Size = new System.Drawing.Size(277, 147);
+            this.btnLogOut.TabIndex = 6;
+            this.btnLogOut.Text = "Log Out";
+            this.btnLogOut.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnLogOut.UseVisualStyleBackColor = true;
+            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
             // 
             // btnPatHistory
             // 
@@ -122,29 +88,47 @@
             this.btnPatHistory.UseVisualStyleBackColor = true;
             this.btnPatHistory.Click += new System.EventHandler(this.btnPatHistory_Click);
             // 
-            // btnLogOut
+            // btnNewPat
             // 
-            this.btnLogOut.FlatAppearance.BorderSize = 0;
-            this.btnLogOut.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnLogOut.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLogOut.ForeColor = System.Drawing.Color.White;
-            this.btnLogOut.Image = ((System.Drawing.Image)(resources.GetObject("btnLogOut.Image")));
-            this.btnLogOut.Location = new System.Drawing.Point(3, 798);
-            this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(277, 147);
-            this.btnLogOut.TabIndex = 6;
-            this.btnLogOut.Text = "Log Out";
-            this.btnLogOut.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
-            this.btnLogOut.UseVisualStyleBackColor = true;
-            this.btnLogOut.Click += new System.EventHandler(this.btnLogOut_Click);
+            this.btnNewPat.FlatAppearance.BorderSize = 0;
+            this.btnNewPat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewPat.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewPat.ForeColor = System.Drawing.Color.White;
+            this.btnNewPat.Image = ((System.Drawing.Image)(resources.GetObject("btnNewPat.Image")));
+            this.btnNewPat.Location = new System.Drawing.Point(3, 223);
+            this.btnNewPat.Name = "btnNewPat";
+            this.btnNewPat.Size = new System.Drawing.Size(277, 108);
+            this.btnNewPat.TabIndex = 4;
+            this.btnNewPat.Text = "New Patient";
+            this.btnNewPat.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnNewPat.UseVisualStyleBackColor = true;
+            this.btnNewPat.Click += new System.EventHandler(this.btnNewPat_Click);
             // 
-            // panelLeft
+            // btnHome
             // 
-            this.panelLeft.BackColor = System.Drawing.Color.DarkCyan;
-            this.panelLeft.Location = new System.Drawing.Point(281, 108);
-            this.panelLeft.Name = "panelLeft";
-            this.panelLeft.Size = new System.Drawing.Size(10, 111);
-            this.panelLeft.TabIndex = 3;
+            this.btnHome.FlatAppearance.BorderSize = 0;
+            this.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHome.Font = new System.Drawing.Font("Century Gothic", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHome.ForeColor = System.Drawing.Color.White;
+            this.btnHome.Image = ((System.Drawing.Image)(resources.GetObject("btnHome.Image")));
+            this.btnHome.Location = new System.Drawing.Point(0, 109);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(280, 108);
+            this.btnHome.TabIndex = 3;
+            this.btnHome.Text = "Home";
+            this.btnHome.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.White;
+            this.panel3.Controls.Add(this.pictureBox1);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(283, 103);
+            this.panel3.TabIndex = 2;
             // 
             // pictureBox1
             // 
@@ -157,11 +141,27 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(286, 587);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1325, 150);
+            this.panel2.TabIndex = 1;
+            // 
+            // panelLeft
+            // 
+            this.panelLeft.BackColor = System.Drawing.Color.DarkCyan;
+            this.panelLeft.Location = new System.Drawing.Point(281, 108);
+            this.panelLeft.Name = "panelLeft";
+            this.panelLeft.Size = new System.Drawing.Size(10, 111);
+            this.panelLeft.TabIndex = 3;
+            // 
             // DoctorUI
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnableAllowFocusChange;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(41)))), ((int)(((byte)(44)))), ((int)(((byte)(51)))));
-            this.ClientSize = new System.Drawing.Size(1608, 948);
+            this.ClientSize = new System.Drawing.Size(1570, 736);
             this.Controls.Add(this.panelLeft);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
@@ -169,7 +169,7 @@
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(62)))), ((int)(((byte)(120)))), ((int)(((byte)(138)))));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "DoctorUI";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "DoctorUI";
             this.Load += new System.EventHandler(this.DoctorUI_Load);
             this.panel1.ResumeLayout(false);
